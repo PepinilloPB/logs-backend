b0fe0628fa3aaa36ac8685ef95c1010e281c2a01
PepinilloPB <edgar.carrasco@ucb.edu.bo>
2022-10-10 20:46:16 -0400

    db

--- build/keys.js
+++ build/keys.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    //DB con MySQL
-    database: {
+    //DB con MySQL Local
+    /*database: {
         host: 'localhost',
         user: 'root',
         password: 'Tricampeon3',
         database: 'beer_pack'
+    }*/
+    //DB con MySQL Clever Cloud
+    database: {
+        host: 'biuxqv7hdqss94i4vlow-mysql.services.clever-cloud.com',
+        user: 'ufotaqb9anu3qhos',
+        password: '1DEr3VAjOx18hhstaorF',
+        database: 'biuxqv7hdqss94i4vlow'
     }
 };
